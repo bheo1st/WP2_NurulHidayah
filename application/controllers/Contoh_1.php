5926865668b1f16a80ad3dfcd12c1bf2546fa428
bheo1st <19200551.bsi.ac.id>
2021-09-13 11:13:44 +0700

    initial commit

---
+++ application/controllers/Contoh_1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh_1 extends CI_controller
+{
+    public function index()
+    {
+        echo "Perkenalkan<br>";
+        echo "Nama Saya Nurul Hidayah. Saya seorang mahasiswa universitas bina sarana informatika<br>";
+        echo "Saya memiliki hobi bermain games";
+    }
+}
