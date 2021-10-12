4f5067b883ac12778d2e39948a03e195764a8ff5
bheo1st <19200551.bsi.ac.id>
2021-10-12 15:20:38 +0700

    Pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,28 @@
+<section>
+    <h1>
+        <?php echo $judul ?>
+    </h1>
+    <h4>
+        Nama
+    </h4>
+    <ul type="disc">
+        <li>Nama Depan : Nurul</li>
+        <li>Nama Belakanng : Hidayah</li>
+    </ul>
+    <br>
+    <h4>
+        alamat
+    </h4>
+    <ul type="none">
+        <li>Jl H terin No 36 Pangakalan Jati Baru</li>
+
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bogor</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Futsal</li>
+    </ul>
+</section>
