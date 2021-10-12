37e0fc33afc81b10ea830c9c31c53f27aa15199f
bheo1st <19200551.bsi.ac.id>
2021-10-12 13:44:54 +0700

    Pertemuan 3

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Latihan1</title>
+    </head>
+
+    <body>
+        Halo Kawan.. Yuk kita belajar web programing ..!!! <br>
+        Nilai 1 = <?=$nilai1;?> <br>
+        Nilai 2 = <?=$nilai2;?> <br>
+        ini hasil pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil;?>
+    </body>
+</html>
